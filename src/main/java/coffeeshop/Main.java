d530fbbf93f4f904394260628e03c3bd192b5432
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-06-21 22:02:02 +0300

    Configure ApplicationContext using AppConfig class

--- src/main/java/coffeeshop/Main.java
+++ src/main/java/coffeeshop/Main.java
@@ -1,7 +1,9 @@
 package coffeeshop;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
     }
 }
